0a84f726d75f1b5b314add05b5e541a8ae0887d7
Andrey <yastrebsov@icloud.com>
2024-08-05 18:14:25 +0300

    implemented logout

--- app/routes/profile/profile.tsx
+++ app/routes/profile/profile.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import {
   json,
   useLoaderData,
+  useFetcher,
   useNavigate,
   useNavigation,
   Link,
@@ -21,6 +23,10 @@ import {
   ListItemButton,
   ListItemIcon,
   Divider,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  Button,
 } from "@mui/material";
 import { TopNavigation } from "~/shared/ui/TopNavigation/TopNavigation";
 import { Loader } from "~/shared/ui/Loader/Loader";
@@ -33,6 +39,7 @@ import { BulletIcon } from "~/shared/icons/BulletIcon";
 
 import { queryClient } from "~/root";
 import { getAccessToken } from "~/preferences/token/token";
+import { clearPreferences } from "~/preferences/preferences";
 import {
   getUserInfo,
   getUserInfoKeys,
@@ -54,12 +61,21 @@ export async function clientLoader() {
   }
 }
 
+export async function clientAction() {
+  await clearPreferences();
+
+  return json({ status: "ok" });
+}
+
 export default function Profile() {
   const theme = useTheme();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
   const data = useLoaderData<typeof clientLoader>();
+  const fetcher = useFetcher();
+
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   return (
     <>
@@ -160,6 +176,7 @@ export default function Profile() {
             }}
           >
             <ListItemButton
+              disabled
               component={Link}
               to="/"
               sx={{
@@ -211,6 +228,7 @@ export default function Profile() {
             }}
           >
             <ListItemButton
+              disabled
               component={Link}
               to="/"
               sx={{
@@ -262,8 +280,11 @@ export default function Profile() {
             }}
           >
             <ListItemButton
-              component={Link}
-              to="/"
+              // component={Link}
+              // to="/"
+              onClick={() => {
+                setOpenDialog(true);
+              }}
               sx={{
                 padding: "16px 0px",
                 columnGap: "12px",
@@ -300,6 +321,41 @@ export default function Profile() {
           </ListItem>
         </List>
       </Box>
+
+      <Dialog
+        open={openDialog}
+        onClose={() => {
+          setOpenDialog(false);
+        }}
+        sx={{
+          "& .MuiDialog-paper": {
+            borderRadius: "8px",
+          },
+        }}
+      >
+        <DialogTitle>{t("Profile.dialog", { context: "title" })}</DialogTitle>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              setOpenDialog(false);
+            }}
+          >
+            {t("Profile.dialog", { context: "no" })}
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              fetcher.submit(JSON.stringify({ logout: "logout" }), {
+                method: "POST",
+                encType: "application/json",
+              });
+            }}
+          >
+            {t("Profile.dialog", { context: "yes" })}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
