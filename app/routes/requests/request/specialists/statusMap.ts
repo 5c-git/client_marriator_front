bf9abca69113688f504d148f5ddffd6d08f2ea6c
Andrey <yastrebsov@icloud.com>
2025-09-29 12:01:21 +0300

    Added "cancelled" status to specialists

--- app/routes/requests/request/specialists/statusMap.ts
+++ app/routes/requests/request/specialists/statusMap.ts
@@ -1,9 +1,10 @@
 export const statusCodeMap = {
   1: { value: "notAccepted", color: "var(--mui-palette-Grey_1)" },
   2: { value: "accepted", color: "var(--mui-palette-Blue)" },
-  3: { value: "declined", color: "var(--mui-palette-Red)" },
+  3: { value: "declined", color: "var(--mui-palette-Grey_2)" },
   4: { value: "consideration", color: "var(--mui-palette-Corp_1)" },
   5: { value: "work", color: "var(--mui-palette-Green)" },
+  6: { value: "canceled", color: "var(--mui-palette-Red)" },
 } as const;
 
 export const statusValueMap = {
@@ -12,4 +13,5 @@ export const statusValueMap = {
   [statusCodeMap[3].value]: 3,
   [statusCodeMap[4].value]: 4,
   [statusCodeMap[5].value]: 5,
+  [statusCodeMap[6].value]: 6,
 };
